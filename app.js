bef3fa1eb9b1969abcfbd583814bc20b9562c6fa
shubham6369 <shubham67257@gmail.com>
2026-06-03 19:28:46 +0530

    feat(animation): slow down fly-to-cart animation to 1.3s and make flying image scale larger

--- app.js
+++ app.js
@@ -114,17 +114,17 @@ const animateFlyToCart = (img, cartBtn, callback) => {
     requestAnimationFrame(() => {
         flyEl.style.top = `${targetTop}px`;
         flyEl.style.left = `${targetLeft}px`;
-        flyEl.style.width = '30px';
-        flyEl.style.height = '30px';
-        flyEl.style.opacity = '0.05';
-        flyEl.style.transform = 'scale(0.1) rotate(540deg)';
+        flyEl.style.width = '50px';
+        flyEl.style.height = '50px';
+        flyEl.style.opacity = '0.15';
+        flyEl.style.transform = 'scale(0.65) rotate(540deg)';
     });
     
     // Clean up elements and execute cart sync callback when animation completes
     setTimeout(() => {
         flyEl.remove();
         callback();
-    }, 800);
+    }, 1300);
 };
 
 const addToCart = (e) => {
